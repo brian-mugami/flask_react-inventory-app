a07899ce4855822d83d8afc976a3fe15fd82cba8
Mugami <brianmugz1@gmail.com>
2023-04-08 22:45:13 +0300

    db changes

--- backend/invapp/models/masters/accountsmodel.py
+++ backend/invapp/models/masters/accountsmodel.py
@@ -8,7 +8,7 @@ class AccountModel(db.Model):
     account_name = db.Column(db.String(80), nullable=False, unique=True)
     account_description = db.Column(db.String(256))
     account_type = db.Column(db.Enum("cash", "credit","none" ,name="account_types"), default="none")
-    account_number = db.Column(db.Integer, nullable=False, unique=True)
+    account_number = db.Column(db.String(100), nullable=False)
     account_category = db.Column(db.String(100), nullable=False)
     is_active = db.Column(db.Boolean, default=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
@@ -16,6 +16,9 @@ class AccountModel(db.Model):
     date_unarchived = db.Column(db.DateTime)
     is_archived = db.Column(db.Boolean, default=False)
 
+    __table_args__ = (
+        db.UniqueConstraint('account_name', "account_number", 'account_category', name="account_unique_check"),
+    )
 
     category = db.relationship("CategoryModel", back_populates="account", passive_deletes=True, lazy="dynamic")
     supplier = db.relationship("SupplierModel", back_populates="account", lazy="dynamic", passive_deletes=True)
