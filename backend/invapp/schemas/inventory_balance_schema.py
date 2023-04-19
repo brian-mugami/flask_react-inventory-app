c76ff0b336a7157d5df2ecf86d8c1f2b60fa2d54
Mugami <brianmugz1@gmail.com>
2023-04-19 08:55:55 +0300

    misc  transactions

--- backend/invapp/schemas/inventory_balance_schema.py
+++ backend/invapp/schemas/inventory_balance_schema.py
@@ -1,10 +1,31 @@
 from marshmallow import Schema, fields
 
-class InventoryBalanceSchema(Schema):
+from invapp.schemas.salesschema import Receipt
+
+class ItemSchema(Schema):
+    item_name = fields.String()
+
+class InvoiceSchema(Schema):
+    invoice_number = fields.String()
+    amount = fields.Float()
+
+class PlainInventoryBalanceSchema(Schema):
     id = fields.Int(dump_only=True, required=True)
     quantity = fields.Int(required=True)
-    unit_cost = fields.Float(required=True)
-    date = fields.Date(dump_only=True)
-    item_id = fields.Int(required=True)
+    unit_cost = fields.Float()
+    date = fields.Date(dump_only=True, required=True)
+    item_id = fields.Int(required=True, dump_only=True)
+    item_name = fields.String(required=True)
     receipt_id = fields.Int()
     invoice_id = fields.Int()
+
+class InventoryBalanceSchema(PlainInventoryBalanceSchema):
+    item = fields.Nested(ItemSchema(), dump_only=True)
+    invoice = fields.Nested(InvoiceSchema(), dump_only=True)
+    receipt = fields.Nested(Receipt(), dump_only=True)
+
+class BalanceSearchSchema(Schema):
+    item_name = fields.String(required=True)
+
+
+
